5fb27eddd10f28d6d9ba8dfdee8c86f5eadf04aa
Jonathan James <jjamesnwa1011@gmail.com>
2018-02-01 20:11:34 -0800

    added comment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True #displays runtime errors in terminal
+#This next line will differ based on how you config your db on your machine locally
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://attendenceapp:attendenceapp@localhost:8889/attendenceapp'
 app.config['SQLALCHEMY_ECHO'] = True
 
