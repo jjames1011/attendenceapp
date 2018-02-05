27d5f750055b3819fec760d17b846737fe4b3e54
Jonathan James <jjamesnwa1011@gmail.com>
2018-02-04 20:55:27 -0800

    added roster_student_table

--- models.py
+++ models.py
@@ -4,12 +4,13 @@ class Roster(Model.db):
     id = db.Column(db.Integer, primary_key=True)
     course_Name = db.Column(db.String(80))
     students = db.Column(db.Integer, ForeignKey='student.id')
+    relationship = db.relationship(Roster_Student_Relationship, backref='roster')
 
 class Student(Model.db):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     notes = db.Column(db.String(250))
-    rosters = db.relationship('Roster', backref='student')
+    rosters = db.relationship('Roster_Student_Relationship', backref='student')
 
 
     def __init__(self, name, notes):
@@ -18,3 +19,13 @@ class Student(Model.db):
 
     def __repr__(self):
         return '<Student %r' self.name
+
+class Roster_Student_Relationship(Model.db):
+    '''This is a table to represent the many-to-many relationship between a roster and a student. '''
+    id = db.Column(db.Integer, primary_key=True)
+    roster_Id = db.Column(db.Integer, ForeignKey='roster.id')
+    student_Id = db.Column(db.Integer, ForeignKey='student.id')
+
+    def __init__(self, roster_Id, player_id):
+        self.roster_Id = roster_Id
+        self.player_id = player_Id
