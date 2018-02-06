ea9433202cfa74d9271c773baf3e223fab7c3cd2
Jonathan James <jjamesnwa1011@gmail.com>
2018-02-05 21:15:08 -0800

    added html templates list_students.html single_roster.html student_profile.html, added single_roster route handler in main.py to query db list students in a specific roster

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def index():
     title = 'Rosters:'
     return render_template('list_rosters.html', rosters=rosters, title=title)
 
+@app.route('/single_roster')
+def single_roster():
+    roster_Id = request.args.get('roster_id')
+    roster = Roster.query.filter_by(id=roster_Id).first()
+    students = Roster_Student_Relationship.query.filter_by(roster_Id=roster_Id).all()
+    return render_template('single_roster.html', students=students,title=roster.course_Name)
+
+
 if __name__ == '__main__':
     app.run()
