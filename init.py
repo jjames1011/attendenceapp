7e685d145fbb138ab3a914d372829e89b7109907
Thai Nguyen <thaindq@outlook.com>
2018-02-16 21:48:05 -0800

    Updated init.py for deleting all constraints before droping tables

--- init.py
+++ init.py
@@ -1,9 +1,58 @@
+from sqlalchemy.engine import reflection
+from sqlalchemy import create_engine
+from sqlalchemy.schema import (
+    MetaData,
+    Table,
+    DropTable,
+    ForeignKeyConstraint,
+    DropConstraint,
+)
 from app import app, db
 from models import *
 
 
+def drop_all(db):
+    # From http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
+
+    conn=db.engine.connect()
+
+    # the transaction only applies if the DB supports
+    # transactional DDL, i.e. Postgresql, MS SQL Server
+    trans = conn.begin()
+
+    inspector = reflection.Inspector.from_engine(db.engine)
+
+    # gather all data first before dropping anything.
+    # some DBs lock after things have been dropped in 
+    # a transaction.
+    metadata = MetaData()
+
+    tbs = []
+    all_fks = []
+
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(
+                ForeignKeyConstraint((),(),name=fk['name'])
+                )
+        t = Table(table_name,metadata,*fks)
+        tbs.append(t)
+        all_fks.extend(fks)
+
+    for fkc in all_fks:
+        conn.execute(DropConstraint(fkc))
+
+    for table in tbs:
+        conn.execute(DropTable(table))
+
+    trans.commit()
+
+
 with app.app_context():
-    db.drop_all()
+    drop_all(db)    
     db.create_all()
 
     # Start to create default data
@@ -27,14 +76,9 @@ with app.app_context():
     # For getting student and roster id without committing
     db.session.flush()
 
-    roster_student_1 = Roster_Student_Relationship(roster1.id, student1.id)
-    roster_student_2 = Roster_Student_Relationship(roster1.id, student2.id)
-    roster_student_3 = Roster_Student_Relationship(roster1.id, student3.id)
-    roster_student_4 = Roster_Student_Relationship(roster1.id, student4.id)
-
-    db.session.add(roster_student_1)
-    db.session.add(roster_student_2)
-    db.session.add(roster_student_3)
-    db.session.add(roster_student_4)
+    roster1.students.append(student1)
+    roster1.students.append(student2)
+    roster1.students.append(student3)
+    roster1.students.append(student4)
 
     db.session.commit()
