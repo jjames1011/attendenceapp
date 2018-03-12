bae68a0cd3628b3156f630edeefac5a7f187c067
dpryor <santonix67@hotmail.com>
2018-03-12 11:16:32 -0700

    Changed the app config back

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True #displays runtime errors in terminal
 #This next line will differ based on how you config your db on your machine locally
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://attendenceapp:attendenceapp@localhost:8889/attendenceapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = False
 
 db = SQLAlchemy(app)
